6129c5138b0e058e35c7d2644c99540c19992c44
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-03 14:19:28 +0200

    pnjs

--- Scripts/ZIndexEtColl.gd
+++ Scripts/ZIndexEtColl.gd
@@ -7,6 +7,7 @@ var zone_coll = preload("res://Scenes/ZoneCollPnj.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	z_index = global_position.y/2
+	z_as_relative = false
 	var zone_c = zone_coll.instance()
 	add_child(zone_c)
 
