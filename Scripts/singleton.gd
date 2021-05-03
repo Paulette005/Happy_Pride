c39de419ce59a59c69066e14260ea8af86cd57b6
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-03 21:22:11 +0200

    ld

--- Scripts/singleton.gd
+++ Scripts/singleton.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-var posPlayerSingleton = Vector2 (120, 420)
+var posPlayerSingleton = Vector2 (247.915, -26)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
