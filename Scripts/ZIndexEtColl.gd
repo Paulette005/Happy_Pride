918940e1742c10874c45bf3c6eba40906a7e37a7
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-03 18:56:38 +0200

    integration pnj

--- Scripts/ZIndexEtColl.gd
+++ Scripts/ZIndexEtColl.gd
@@ -2,14 +2,15 @@ extends Sprite
 
 
 var zone_coll = preload("res://Scenes/ZoneCollPnj.tscn")
-
+export var add_collision = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	z_index = global_position.y/2
 	z_as_relative = false
-	var zone_c = zone_coll.instance()
-	add_child(zone_c)
+	if add_collision :
+		var zone_c = zone_coll.instance()
+		add_child(zone_c)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
