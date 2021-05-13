45ab8e567086f81ba863c7e469c2c947295717a3
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-13 16:05:49 +0200

    musiques et jasmine

--- Scripts/effet-enceinte.gd
+++ Scripts/effet-enceinte.gd
@@ -2,6 +2,7 @@ extends AnimatedSprite
 
 var i = 0
 export var tempo = 62
+export var is_pink_block = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,9 +10,12 @@ func _ready():
 	$Timer.start()
 	print ($Timer.wait_time)
 
+
 func on_timer_timeout():
 	set_frame(i)
 	visible = !visible
 	i += 1
-	if i >= 11 :
+	if !is_pink_block && i >= 11 :
+		i = 0
+	if is_pink_block && i >= 4 :
 		i = 0
