86b0874ab02563f5f278efcb0e9979900aea893b
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-27 22:28:23 +0200

    ld

--- Scripts/singleton.gd
+++ Scripts/singleton.gd
@@ -3,7 +3,7 @@ extends Node2D
 #var posPlayerSingleton = Vector2 (247.915, -26)
 
 var langues = 0
-
+var posPlayerSingleton
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
