9a0d713278c47e351df6264b7840b949e70f5da4
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-30 19:05:00 +0200

    credits

--- Scripts/BoutonsMenu.gd
+++ Scripts/BoutonsMenu.gd
@@ -12,7 +12,12 @@ func _on_Options_pressed():
 	get_tree().change_scene("res://Scenes/Menu/Options.tscn")
 
 func _on_Credits_pressed():
-	get_tree().change_scene("res://Scenes/Menu/Credits.tscn")
+	get_node("/root/Control/Credits").visible = true
+	get_node("/root/Control/Credits/Retour").grab_focus()
 
 func _on_Quitter_pressed():
 	get_tree().quit()
+	
+func _on_retour_credit_pressed():
+	get_node("/root/Control/Credits").visible = false
+	get_node("/root/Control/VBoxContainer/Commencer").grab_focus()
