d370e44fa2807394355e9253ecd8c9937cfdfd73
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-05-30 21:42:36 +0200

    traduction credits

--- Scripts/0_LangueMenu.gd
+++ Scripts/0_LangueMenu.gd
@@ -22,4 +22,10 @@ func chargement_langue():
 		$VBoxContainer/Options/Options.bbcode_text = "[center]Options[/center]"
 		$VBoxContainer/Credits/Credits.bbcode_text = "[center]Credits[/center]"
 		$VBoxContainer/Quitter/Quitter.bbcode_text = "[center]Quit[/center]"
-		
+		get_node("/root/Control/Credits/HBoxContainer/paulette/role fr").visible = false
+		get_node("/root/Control/Credits/HBoxContainer/paulette/role en").visible = true
+		get_node("/root/Control/Credits/HBoxContainer/Axell/role fr").visible = false
+		get_node("/root/Control/Credits/HBoxContainer/Axell/role en").visible = true
+		get_node("/root/Control/Credits/HBoxContainer/Rose/role fr").visible = false
+		get_node("/root/Control/Credits/HBoxContainer/Rose/role en").visible = true		
+		$Credits/Retour/retour.bbcode_text= "[center]Back[/center]"
