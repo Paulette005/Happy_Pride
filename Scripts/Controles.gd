ab38c776911e9bb467556b95935ec97d012a31f5
Paulette005 <paulette@zaclys.net>
2021-05-31 15:20:52 +0200

    presser une touche

--- Scripts/Controles.gd
+++ Scripts/Controles.gd
@@ -10,12 +10,11 @@ func _ready():
 	if singleton.langues == 1 :
 		$"controles fr".visible = false
 		$"controles en".visible = true
+		$indications.bbcode_text = "[rainbow]Press any key to start[/rainbow]"
 	if singleton.clavier == "qwerty":
 		$qwerty.visible = true
 
 func _input(event):
 	if event is InputEventKey and event.pressed:
 		get_tree().change_scene("res://Scenes/TeteCortege.tscn")
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
