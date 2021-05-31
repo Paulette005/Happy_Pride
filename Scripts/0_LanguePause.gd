4cf4f7866f1f2516f19e389891a9fab9f341bab5
Paulette005 <paulette@zaclys.net>
2021-05-31 20:32:06 +0200

    menu pause et affichage langues

--- Scripts/0_LanguePause.gd
+++ Scripts/0_LanguePause.gd
@@ -13,11 +13,13 @@ func chargement_langue():
 	if langues == 0:
 		$Pause/Reprendre/Reprendre.bbcode_text = "[center]Reprendre[/center]"
 		$Pause/Options/Options.bbcode_text = "[center]Options[/center]"
+		$Pause/Retour/Retour.bbcode_text = "[center]Retour menu[/center]"
 		$Pause/Quitter/Quitter.bbcode_text = "[center]Quitter[/center]"
 		
 	if langues == 1:
 		$Pause/Reprendre/Reprendre.bbcode_text = "[center]Resume[/center]"
 		$Pause/Options/Options.bbcode_text = "[center]Options[/center]"
+		$Pause/Retour/Retour.bbcode_text = "[center]Back to menu[/center]"
 		$Pause/Quitter/Quitter.bbcode_text = "[center]Quit[/center]"
 		
 func _on_Reprendre_pressed():
@@ -31,3 +33,6 @@ func _on_Options_pressed():
 	Pause.visible = false
 	Options.visible = true
 	get_node("/root/Node2D/CanvasLayer/Options/VBoxContainer/Langues").grab_focus()
+
+func _on_Retour_pressed():
+	get_tree().change_scene("res://Scenes/Menu/Menu.tscn")
