2932e1e9b43e8c46d18e65f097cc7cdf5047d455
Paulette005 <paulette@zaclys.net>
2021-05-31 18:51:51 +0200

    verifications

--- Scripts/ChangementdeScene.gd
+++ Scripts/ChangementdeScene.gd
@@ -9,8 +9,11 @@ onready var posPlayer = get_node("/root/Node2D/Player")
 
 func _ready():
 	connect("body_entered", self, "on_body_entered")
+	#indicationDirection = posPlayer.direction
 	
 func on_body_entered(body):
 	if body.name == "Player":
+		indicationDirection = posPlayer.direction
+		print("Direction: ", posPlayer.direction)
 		get_node("/root/Singleton").posPlayerSingleton = Vector2 (posX, posPlayer.get_position().y)
 		get_tree().change_scene("res://Scenes/"+nomScene+".tscn")
