291b8b25180145aa75678b087bb0f5c2a580ca1c
Axell B <56314049+AxellBoue@users.noreply.github.com>
2021-06-01 14:01:23 +0200

    Update Deplacements.gd

--- Scripts/Deplacements.gd
+++ Scripts/Deplacements.gd
@@ -23,7 +23,7 @@ func _ready():
 	$AnimatedSprite.connect("animation_finished", self, "chang_anim")
 	$Timer.connect("timeout", self, "actionPlayer")
 	var singleton = get_node("/root/Singleton")
-	#global_position = singleton.posPlayerSingleton
+	global_position = singleton.posPlayerSingleton
 
 func get_input():
 	velocite = Vector2 ()
